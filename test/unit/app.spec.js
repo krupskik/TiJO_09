0def41a3502179d14f7dddb8e2c383c6bd437549
Kamil Krupski <krupski102@gmail.com>
2016-05-04 14:51:28 +0200

    mutation test

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -4,8 +4,18 @@ describe('app', function () {
     var answer = window.app;
 
     describe('calculateArea', function () {
-        it('should should...', function () {
-
+        it('should return Error', function () {
+            expect(answer.calculateArea(5, 2, 4, 'Success', 'Error')).toEqual({area : -3, message : 'Error'});
         });
+        it('should return Error', function () {
+            expect(answer.calculateArea(2, 2, 2, 'Success', 'Success')).toEqual({area : -2, message : 'Success'});
+        });
+        it('should return Error', function () {
+            expect(answer.calculateArea('a', 'b', 'c', 1, 5)).toEqual(false);
+        });
+        it('should return Error', function () {
+            expect(answer.calculateArea('a', 5, 'c', 1, 'Error')).toEqual(false);
+        });
+
     });
 });
